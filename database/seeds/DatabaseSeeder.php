bf9b739ec6a21de63f76728e75664c4056c18987
intro333 <intro333@ya.ru>
2017-10-14 21:46:23 +0300

    отключил сиды(все кроме квот)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //        $this->call(ConfidentialsTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
+//        $this->call(CategoriesTableSeeder::class);
+//        $this->call(ProductsTableSeeder::class);
         $this->call(OrderQuotaTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
     }
 }
