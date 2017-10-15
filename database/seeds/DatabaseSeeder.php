f9c03cf58788c50b437c0ed0a75cd63b75fda877
intro333 <intro333@ya.ru>
2017-10-15 13:14:35 +0300

    fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(ConfidentialsTableSeeder::class);
+        $this->call(ConfidentialsTableSeeder::class);
 //        $this->call(CategoriesTableSeeder::class);
 //        $this->call(ProductsTableSeeder::class);
         $this->call(OrderQuotaTableSeeder::class);
